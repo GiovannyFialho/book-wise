5e9690cf750370da804699cf9e9c7c1ee9391763
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-15 14:25:54 -0300

    ajuste na pagina de perfil

--- components/ProfileContent/index.tsx
+++ components/ProfileContent/index.tsx
@@ -5,8 +5,10 @@ import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import "dayjs/locale/pt-br";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { MagnifyingGlass } from "phosphor-react";
+import Link from "next/link";
+import { CaretLeft, MagnifyingGlass } from "phosphor-react";
 import { useMemo, useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa6";
 
@@ -36,6 +38,7 @@ interface ProfileContentProps {
 
 export function ProfileContent({ userId }: ProfileContentProps) {
   const [search, setSearch] = useState("");
+  const { data: session } = useSession();
 
   const { data: profile, isLoading } = useQuery<ProfileQuery>({
     queryKey: ["user", userId],
@@ -65,89 +68,100 @@ export function ProfileContent({ userId }: ProfileContentProps) {
   };
 
   return (
-    <div className="mt-10 flex flex-1 gap-16">
-      <section className="w-full">
-        <div className="flex flex-1 flex-col gap-8">
-          <div className="flex min-h-12 items-center gap-2 rounded-md border border-gray-500 px-5 py-3">
-            <input
-              type="text"
-              placeholder="Buscar livro avaliado"
-              className="w-full bg-transparent"
-              onChange={({ target }) => setSearch(target.value)}
-            />
+    <>
+      {userId !== session?.user.id && (
+        <Link href="/" className="mt-5 flex items-center gap-3">
+          <CaretLeft size={20} />
+          Voltar
+        </Link>
+      )}
 
-            <MagnifyingGlass size={20} className="text-gray-500" />
-          </div>
+      <div
+        className={`flex flex-1 gap-16 ${userId !== session?.user.id ? "mt-5" : "mt-10"}`}
+      >
+        <section className="w-full">
+          <div className="flex flex-1 flex-col gap-8">
+            <div className="flex min-h-12 items-center gap-2 rounded-md border border-gray-500 px-5 py-3">
+              <input
+                type="text"
+                placeholder="Buscar livro avaliado"
+                className="w-full bg-transparent"
+                onChange={({ target }) => setSearch(target.value)}
+              />
 
-          <div className="flex flex-col gap-6">
-            {filteredRatings?.map((rateBook) => (
-              <div key={rateBook.id} className="flex flex-col gap-2">
-                <p className="text-sm text-gray-300">
-                  {dayjs().to(dayjs(rateBook.created_at))}
-                </p>
+              <MagnifyingGlass size={20} className="text-gray-500" />
+            </div>
 
-                <div className="flex w-full flex-col gap-6 rounded-lg bg-gray-700 p-6">
-                  <div className="flex gap-6">
-                    <Image
-                      className="w-24 rounded-md"
-                      src="https://m.media-amazon.com/images/I/91M9xPIf10L._SY466_.jpg"
-                      width={98}
-                      height={134}
-                      alt={rateBook.book.name}
-                    />
+            <div className="flex flex-col gap-6">
+              {filteredRatings?.map((rateBook) => (
+                <div key={rateBook.id} className="flex flex-col gap-2">
+                  <p className="text-sm text-gray-300">
+                    {dayjs().to(dayjs(rateBook.created_at))}
+                  </p>
 
-                    <div className="flex flex-col justify-between">
-                      <div>
-                        <h3 className="text-lg font-bold">
-                          {rateBook.book.name}
-                        </h3>
-                        <p className="text-sm text-gray-400">
-                          {rateBook.book.author}
-                        </p>
-                      </div>
+                  <div className="flex w-full flex-col gap-6 rounded-lg bg-gray-700 p-6">
+                    <div className="flex gap-6">
+                      <Image
+                        className="w-24 rounded-md"
+                        src="https://m.media-amazon.com/images/I/91M9xPIf10L._SY466_.jpg"
+                        width={98}
+                        height={134}
+                        alt={rateBook.book.name}
+                      />
 
-                      <div className="flex gap-1">
-                        {Array.from({ length: 5 }, (_, index) => {
-                          if (index + 1 <= rateBook.rate) {
-                            return (
-                              <FaStar
-                                key={index}
-                                className="text-purple-100"
-                                size={24}
-                              />
-                            );
-                          } else {
-                            return (
-                              <FaRegStar
-                                key={index}
-                                className="text-purple-100"
-                                size={24}
-                              />
-                            );
-                          }
-                        })}
+                      <div className="flex flex-col justify-between">
+                        <div>
+                          <h3 className="text-lg font-bold">
+                            {rateBook.book.name}
+                          </h3>
+                          <p className="text-sm text-gray-400">
+                            {rateBook.book.author}
+                          </p>
+                        </div>
+
+                        <div className="flex gap-1">
+                          {Array.from({ length: 5 }, (_, index) => {
+                            if (index + 1 <= rateBook.rate) {
+                              return (
+                                <FaStar
+                                  key={index}
+                                  className="text-purple-100"
+                                  size={24}
+                                />
+                              );
+                            } else {
+                              return (
+                                <FaRegStar
+                                  key={index}
+                                  className="text-purple-100"
+                                  size={24}
+                                />
+                              );
+                            }
+                          })}
+                        </div>
                       </div>
                     </div>
+
+                    <p className="text-sm text-gray-300">
+                      {rateBook.description}
+                    </p>
                   </div>
-
-                  <p className="text-sm text-gray-300">
-                    {rateBook.description}
-                  </p>
                 </div>
-              </div>
-            ))}
-
-            {!filteredRatings ||
-              (filteredRatings.length <= 0 && (
-                <p className="text-sm">Nenhum resultado encontrado</p>
               ))}
-          </div>
-        </div>
-      </section>
 
-      <aside className="h-auto w-full max-w-[300px]">
-        <AsideProfile {...asideProfileData} isLoading={isLoading} />
-      </aside>
-    </div>
+              {!filteredRatings ||
+                (filteredRatings.length <= 0 && (
+                  <p className="text-sm">Nenhum resultado encontrado</p>
+                ))}
+            </div>
+          </div>
+        </section>
+
+        <aside className="h-auto w-full max-w-[300px]">
+          <AsideProfile {...asideProfileData} isLoading={isLoading} />
+        </aside>
+      </div>
+    </>
   );
 }
